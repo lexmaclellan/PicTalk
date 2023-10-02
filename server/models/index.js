89693e80e9da8feff1c384b47159ad2bde1fe1a5
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-02 20:21:16 -0300

    Moved starter code to main folder

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Post = require('./Post');
+const Matchup = require('./Matchup');
+const Tech = require('./Tech')
+
+module.exports = { User, Post, Matchup, Tech };
