3d615c4a63571d4225208c3b6a11678b95c62bab
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-03 20:12:13 -0300

    Updated model to add friends and posts

--- server/models/User.js
+++ server/models/User.js
@@ -1,18 +1,39 @@
 const { Schema, model } = require('mongoose')
 
+const validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    return re.test(email)
+};
+
 const userSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        maxlength: 50
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        validate: [validateEmail, 'Please fill a valid email address']
     },
     password: {
         type: String,
         required: true
-    }
+    },
+    posts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Post'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
 })
 
 const User = model('User', userSchema)
