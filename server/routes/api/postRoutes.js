bc9b084c2ba92cd5a36bc4b9004ba3a017a0468f
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-04 22:29:48 -0300

    Routes for comments and user posts

--- server/routes/api/postRoutes.js
+++ server/routes/api/postRoutes.js
@@ -3,12 +3,19 @@ const router = require('express').Router()
 const {
     getAllPosts,
     getOnePost,
+    getPostsFromUser,
     createPost,
     updatePost,
-    deletePost
+    deletePost,
+    getComments,
+    addComment,
+    removeComment
 } = require('../../controllers/postController')
 
 router.route('/').get(getAllPosts).post(createPost)
 router.route('/:postId').get(getOnePost).put(updatePost).delete(deletePost)
+router.route('/:username/posts').get(getPostsFromUser)
+router.route('/:postId/comments').get(getComments).post(addComment)
+router.route('/:postId/comments/:commentId').delete(removeComment)
 
 module.exports = router
