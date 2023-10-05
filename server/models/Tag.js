588eddc4d850e9c8b99e4e254cfaac5aa602d8d3
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-05 20:02:38 -0300

    Removed unnecessary field

--- server/models/Tag.js
+++ server/models/Tag.js
@@ -1,10 +1,6 @@
 const { Schema, Types } = require('mongoose')
 
 const tagSchema = new Schema({
-    tagId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId()
-    },
     name: {
         type: String,
         required: true,
