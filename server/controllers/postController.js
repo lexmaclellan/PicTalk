31b94f13338581ebbf8edb8fa5a0312424f5fae3
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-04 21:33:18 -0300

    Linked posts to users, added comments

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,4 +1,4 @@
-const { Post } = require('../models')
+const { Post, User } = require('../models')
 
 module.exports = {
 
@@ -28,6 +28,16 @@ module.exports = {
     async createPost(req, res) {
         try {
             const post = await Post.create(req.body)
+            const user = await User.findOneAndUpdate(
+                { name: req.body.username },
+                { $push: { posts: post._id } },
+                { new: true }
+            )
+
+            if (!user) {
+                return res.status(404).json({ message: 'Invalid user ID.' })
+            }
+
             res.json(post)
         } catch (err) {
             res.status(500).json(err)
@@ -60,7 +70,53 @@ module.exports = {
                 return res.status(404).json({ message: 'No post found with that ID.' })
             }
 
+            const user = await User.findByIdAndUpdate(
+                { username: post.username },
+                { $pull: { posts: req.params.postId } },
+                { new: true }
+            )
+
+            if (!user) {
+                res.json({ message: 'No user found with that post.' })
+            }
+
             res.json({ message: 'Post deleted.' })
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+
+    async addComment(req, res) {
+        try {
+            const comment = await Post.findByIdAndUpdate(
+                req.params.postId,
+                { $addToSet: { comments: req.body } },
+                { runValidators: true, new: true }
+            )
+
+            if (!comment) {
+                return res.status(404).json({ message: 'No post found with that ID.' })
+            }
+
+            res.json(comment)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+
+    async removeComment(req, res) {
+        try {
+            const comment = await Post.findByIdAndUpdate(
+                req.params.thoughtId,
+                { $pull: { comments: { commentId:req.params.commentId } } },
+                { runValidators: true, new: true }
+            )
+
+            if (!comment) {
+                return res.status(404).json({ message: 'No post found with that ID.' })
+            }
+
+            res.json(comment)
         } catch (err) {
             res.status(500).json(err)
         }
--- server/models/Post.js
+++ server/models/Post.js
@@ -1,10 +1,25 @@
 const { Schema, model } = require('mongoose')
+const commentSchema = require('./Comment')
+const tagSchema = require('./Tag')
 
 const postSchema = new Schema({
   content: {
     type: String,
     required: true,
-  }
+    maxlength: 1000,
+    minlength: 1
+  },
+  username: {
+    type: String,
+    required: true,
+    maxlength: 50
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  comments: [commentSchema],
+  tags: [tagSchema]
 })
 
 const Post = model('Post', postSchema)
