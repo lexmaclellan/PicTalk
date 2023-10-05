7c6d20eb71a236b0fd4c7e0ec230d9154bb90b37
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-05 18:42:15 -0300

    Added user posts route

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -7,11 +7,15 @@ const {
     updateUser,
     deleteUser,
     addFriend,
-    removeFriend
+    removeFriend,
+    getPostsFromUser,
+    getPostsFromFriends
 } = require('../../controllers/userController')
 
 router.route('/').get(getAllUsers).post(createUser)
 router.route('/:userId').get(getOneUser).put(updateUser).delete(deleteUser)
 router.route('/:userId/friends/:friendId').put(addFriend).delete(removeFriend)
+router.route('/:username/posts').get(getPostsFromUser)
+router.route('/:username/friends').get(getPostsFromFriends)
 
 module.exports = router
