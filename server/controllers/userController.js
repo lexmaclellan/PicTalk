090f2051765fc95cc200584e16dc345b7eb6402f
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-10 19:10:22 -0300

    Posts from friends sorting fixed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -141,8 +141,8 @@ module.exports = {
             sortByDate = (a, b) => {
                 const dateA = new Date(a.createdAt)
                 const dateB = new Date(b.createdAt)
-                if (dateA > dateB) return 1
-                else if (dateA < dateB) return -1
+                if (dateA < dateB) return 1
+                else if (dateA > dateB) return -1
                 return 0
             }
 
