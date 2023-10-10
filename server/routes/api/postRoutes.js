3b09bc6b895a71c2c70fd16c142d210bec764901
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-09 22:06:04 -0300

    Added like routes

--- server/routes/api/postRoutes.js
+++ server/routes/api/postRoutes.js
@@ -8,12 +8,15 @@ const {
     deletePost,
     getComments,
     addComment,
-    removeComment
+    removeComment,
+    addLike,
+    removeLike
 } = require('../../controllers/postController')
 
 router.route('/').get(getAllPosts).post(createPost)
 router.route('/:postId').get(getOnePost).put(updatePost).delete(deletePost)
 router.route('/:postId/comments').get(getComments).post(addComment)
 router.route('/:postId/comments/:commentId').delete(removeComment)
+router.route('/:postId/likes/:username').post(addLike).delete(removeLike)
 
 module.exports = router
